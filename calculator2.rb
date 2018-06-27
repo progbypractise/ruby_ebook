f62a9a840870c2dab7a087d4a6902dbbac5ca95e
progbypractise <40620738+progbypractise@users.noreply.github.com>
2018-06-27 00:55:05 -0500

    Create calculator2.rb

---
+++ calculator2.rb
@@ -0,0 +1,10 @@
+puts "First number:"
+first = gets.to_f
+puts "Second number:"
+second = gets.to_f
+puts "#{first}+#{second}=#{first+second}"
+puts "#{first}-#{second}=#{first-second}"
+puts "#{first}*#{second}=#{first*second}"
+puts "#{first}÷#{second}=#{first/second}"
+puts "#{first}mod#{second}=#{first%second}"
+puts "#{first}exp#{second}=#{first**second}"
