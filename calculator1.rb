8d385e17f9ef7e531d29cf2bdfffc6af45a647a8
progbypractise <40620738+progbypractise@users.noreply.github.com>
2018-06-27 00:53:03 -0500

    Rename calc1.rb to calculator1.rb

---
+++ calculator1.rb
@@ -0,0 +1,10 @@
+puts "First number:"
+first = gets.to_i
+puts "Second number:"
+second = gets.to_i
+puts "#{first}+#{second}=#{first+second}"
+puts "#{first}-#{second}=#{first-second}"
+puts "#{first}*#{second}=#{first*second}"
+puts "#{first}÷#{second}=#{first/second}"
+puts "#{first}mod#{second}=#{first%second}"
+puts "#{first}exp#{second}=#{first**second}"
