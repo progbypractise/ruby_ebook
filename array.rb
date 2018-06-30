8c82dc79a6941c98d67de4d08711cd44d469e3a8
progbypractise <40620738+progbypractise@users.noreply.github.com>
2018-06-30 03:17:51 -0500

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,8 @@
+bag = ['Brocolli','vitamins','Vogue','Pastry']
+puts "Original: #{bag}"
+bag.delete('Brocolli')
+puts "Brocolli thrown away:#{bag}" 
+bag.push('Spinach')
+puts "Darn it. Spinach added :#{bag}" 
+bag.clear
+puts "Checked out :#{bag}" 
